6c823df4f4a93fea46d04273dae184435687533d
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-04-02 20:56:06 -0600

    fixing Program.cs and Startup.cs conflicts

--- Furnish/Controllers/UserController.cs
+++ Furnish/Controllers/UserController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Furnish.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Furnish.Controllers
 {
-    public class UserController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
-        public IActionResult Index()
+
+        [HttpGet("Admins")]
+        [Authorize]
+        public IActionResult AdminsEndpoint()
         {
-            return View();
+            var currentUser = GetCurrentUser();
+            if (currentUser.Role == "Administrator")
+            {
+                return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+            }
+            return Ok($"Hi {currentUser.GivenName}, you are a {currentUser.Role}");
         }
+
+
+        private User GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new User
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                };
+            }
+            return null;
+        }
+
+        
     }
 }
