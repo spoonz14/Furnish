46439ec67c46b706d3f8a26b9846f20d2231b28c
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-04-03 11:39:23 -0600

    setting up cart feature

--- Furnish/Controllers/CartController.cs
+++ Furnish/Controllers/CartController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Furnish.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Furnish.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private StoreContext context;
+
+        public CartController(StoreContext ctx)
         {
-            return View();
+            context = ctx;
+        }
+
+        [HttpGet]
+        [Route("[controller]s/{id}")]
+        public IActionResult Cart(int id)
+        {
+            if (Request.Cookies.TryGetValue("jwtToken", out string token))
+            {
+                var product = context.Products.FirstOrDefault(p => p.ProductId == id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
+            return BadRequest("Please login to access the cart");
         }
     }
 }
