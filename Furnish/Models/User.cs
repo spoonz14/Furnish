759d29842f5835fb8a4337fec5ee2b508fe0f74d
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-04-04 19:27:46 -0600

    Update User.cs

--- Furnish/Models/User.cs
+++ Furnish/Models/User.cs
@@ -11,6 +11,7 @@ namespace Furnish.Models
         [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
         [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Role is required.")]
         public string Role { get; set; }
