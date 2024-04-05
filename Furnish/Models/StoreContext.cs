52e7a52983a8a0510602d57d6a28f028b6a0e7b0
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-04-04 19:34:28 -0600

    changing images

--- Furnish/Models/StoreContext.cs
+++ Furnish/Models/StoreContext.cs
@@ -15,11 +15,11 @@ namespace Furnish.Models
             base.OnModelCreating(modelBuilder);
 
            modelBuilder.Entity<Product>().HasData(
-                new Product { ProductId = 1, Name = "Navi", CategoryId = "Sofa", Price = 649.99, ImageUrl = "~/images/navi-sofa.jpg" },
+                new Product { ProductId = 1, Name = "Navi", CategoryId = "Sofa", Price = 649.99, ImageUrl = "~/images/sofa.png" },
                 new Product { ProductId = 2, Name = "Serta Peninsula", CategoryId = "Bed", Price = 899.99, ImageUrl = "~/images/serta-peninsula.jpg" },
                 new Product { ProductId = 3, Name = "Mobili Fiver", CategoryId = "Table", Price = 945.99, ImageUrl = "~/images/mobili-fiver.png" },
-                new Product { ProductId = 4, Name = "Ashley Cashton", CategoryId = "Chair", Price = 2299.99, ImageUrl = "~/images/ashley-cashton.png" },
-                new Product { ProductId = 5, Name = "Llappuil", CategoryId = "Sofa", Price = 649.99, ImageUrl = "~/images/llappuil.jpg" },
+                new Product { ProductId = 4, Name = "Ashley Cashton", CategoryId = "Chair", Price = 2299.99, ImageUrl = "~/images/product1" },
+                new Product { ProductId = 5, Name = "Llappuil", CategoryId = "Sofa", Price = 649.99, ImageUrl = "~/images/couch.png" },
                 new Product { ProductId = 6, Name = "Yuhuashi", CategoryId = "Bed", Price = 247.00, ImageUrl = "~/images/yuhuashi.png" }
                 );
 
