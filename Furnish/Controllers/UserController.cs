5a9e4b0ebe7781bcd4e9271fa4474e43a2d72ea2
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-04-04 18:33:22 -0600

    setting up user profile

--- Furnish/Controllers/UserController.cs
+++ Furnish/Controllers/UserController.cs
@@ -1,23 +1,49 @@
 ﻿using Furnish.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace Furnish.Controllers
 {
     public class UserController : Controller
     {
         private StoreContext context;
+        private readonly IConfiguration _config;
 
-        public UserController(StoreContext ctx)
+        public UserController(StoreContext ctx, IConfiguration config)
         {
             context = ctx;
+            _config = config;
         }
 
         [HttpGet]
         public IActionResult Profile()
         {
-            return View();
+            bool isAuthenticated = Request.Cookies.ContainsKey("jwtToken"); // Check if jwtToken exists in cookies
+            ViewBag.IsAuthenticated = isAuthenticated; // Set ViewBag.IsAuthenticated
+
+            if (isAuthenticated)
+            {
+                string token = Request.Cookies["jwtToken"]; // Get the token value
+                var userClaims = ValidateAndDecodeToken(token);
+                if (userClaims != null)
+                {
+                    var currentUser = new User
+                    {
+                        Username = userClaims.FindFirst(ClaimTypes.NameIdentifier)?.Value,
+                        Email = userClaims.FindFirst(ClaimTypes.Email)?.Value,
+                        Role = userClaims.FindFirst(ClaimTypes.Role)?.Value,
+                        Surname = userClaims.FindFirst(ClaimTypes.Surname)?.Value,
+                        GivenName = userClaims.FindFirst(ClaimTypes.GivenName)?.Value
+                    };
+
+                    return View(currentUser);
+                }
+            }
+            return RedirectToAction("Login", "Login");
         }
 
         [HttpGet]
@@ -54,6 +80,24 @@ namespace Furnish.Controllers
             }
 
             return BadRequest(ModelState); // Return error response with ModelState
+        }
+
+        private ClaimsPrincipal ValidateAndDecodeToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+
+            SecurityToken validatedToken;
+            var claimsPrincipal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            }, out validatedToken);
+
+            return claimsPrincipal;
         }
     }
 }
